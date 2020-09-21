d78c16a646a371a083d8cb7ca1ff132dcd6aa7a6
thomas <thomas@kali>
2020-09-21 17:49:45 +0100

    adding scroll top option on whispr topleft corner

--- js/scripts.js
+++ js/scripts.js
@@ -71,3 +71,20 @@ $(function () {
         $(this).addClass('active');
     });
 });
+
+
+
+var btn = $('#buttontop');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
